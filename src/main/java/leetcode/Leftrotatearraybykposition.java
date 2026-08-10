54952f8febb4b4d9344f4c62324e361f4a84c2f9
Revanshtiwari <tiwarirevansh09@gmail.com>
2026-08-10 14:46:26 +0530

    Leetcode #189 que-left rotate by k position soln

---
+++ src/main/java/leetcode/Leftrotatearraybykposition.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class Leftrotatearraybykposition {
+	public static void main(String args[]) {
+		int a[]= {1,2,3,4,5};
+		int i,j,k;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of k");
+		k=sc.nextInt();
+		if(k<0 || k>a.length) {
+			System.out.println("invalid position");
+		return;
+		}for(j=0;j<k;j++) {
+			int first=a[0];
+			for(i=0;i<a.length-1;i++) {
+				a[i]=a[i+1];
+			}
+			a[a.length-1]=first;
+		}
+		System.out.println("array after moving k to left");
+		for(i=0;i<a.length;i++) {
+			System.out.print(a[i]+"");
+		}
+		
+	}
+}
